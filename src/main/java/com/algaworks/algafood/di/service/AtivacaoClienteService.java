fdeeb063dec2b8c70dc37a87415e207323b6bbd9
Fabio Pereira <fabiuhp@msn.com>
2020-12-06 18:39:16 -0300

    Correções de AutoWired

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -11,24 +11,10 @@ public class AtivacaoClienteService {
 
 	@Autowired
 	private Notificador notificador;
-	
-//	@Autowired
-//	public AtivacaoClienteService(Notificador notificador) {
-//		this.notificador = notificador;
-//	}
-//	
-//	public AtivacaoClienteService(String qualquer) {
-//		
-//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		notificador.notificar(cliente, "Seu cadastro  no sistema está ativo!");
 	}
-
-//	@Autowired
-//	public void setNotificador(Notificador notificador) {
-//		this.notificador = notificador;
-//	}
 }
