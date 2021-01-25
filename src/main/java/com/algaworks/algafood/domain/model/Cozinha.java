2444a22344c1d2725c38ddc7b6c6b01edf61333f
Fabio Pereira <fabiuhp@msn.com>
2021-01-25 20:49:44 -0300

    json rootname

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,12 +1,14 @@
 package com.algaworks.algafood.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Objects;
 
+@JsonRootName("gastronomia")
 @Data
 @Entity
 @EqualsAndHashCode
