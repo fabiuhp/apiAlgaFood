5dc65e006d3d2f578547520b921e8fd0d28faae1
Fabio Pereira <fabiuhp@msn.com>
2021-01-25 20:45:20 -0300

    Adequações nas classes de modelo 2

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -11,12 +11,18 @@ import java.util.Objects;
 @Data
 @EqualsAndHashCode
 public class Restaurante {
+    @EqualsAndHashCode.Include
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
-    @Column(name = "taxa_frete")
+    @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
+
+    @ManyToOne
+    @JoinColumn(name = "cozinha_id", nullable = false)
+    private Cozinha cozinha;
 }
