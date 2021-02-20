618286388911961cee06e99619985f399c0fb943
Fabio Pereira <fabiuhp@msn.com>
2021-02-20 10:22:58 -0300

    estadoController PostMapping

--- src/main/java/com/algaworks/algafoodapi/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/EstadoController.java
@@ -1,13 +1,13 @@
 package com.algaworks.algafoodapi.api.controller;
 
+import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafoodapi.domain.model.Estado;
 import com.algaworks.algafoodapi.domain.repository.EstadoRepository;
+import com.algaworks.algafoodapi.domain.service.CadastroEstadoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +17,9 @@ public class EstadoController {
 
     @Autowired
     private EstadoRepository estadoRepository;
+
+    @Autowired
+    private CadastroEstadoService estadoService;
 
     @GetMapping
     public List<Estado> listar() {
@@ -32,5 +35,16 @@ public class EstadoController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> adicionar(@RequestBody Estado estado) {
+        try {
+            estado = estadoService.salvar(estado);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(estado);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/algaworks/algafoodapi/domain/service/CadastroEstadoService.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafoodapi.domain.service;
+
+import com.algaworks.algafoodapi.domain.model.Estado;
+import com.algaworks.algafoodapi.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroEstadoService {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public Estado salvar(Estado estado) {
+        return estadoRepository.salvar(estado);
+    }
+}
